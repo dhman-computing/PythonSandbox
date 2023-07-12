3339b35593475a987fab22488e6f2d029fde8697
dhman-computing <78009274+dhman-computing@users.noreply.github.com>
2023-07-12 12:05:44 +0000

    Updated urllib1.py

--- Web Scraper/urllib1.py
+++ Web Scraper/urllib1.py
@@ -3,8 +3,8 @@ import urllib.request
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 
-# for line in fhand:
-#     print(line.decode().strip())
+for line in fhand:
+    print(line.decode().strip())
 
 # print(dir(fhand))
-print(fhand.getheaders)
+# print(fhand.getheaders)
